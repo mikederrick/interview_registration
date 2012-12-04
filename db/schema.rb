ad054b729f36b439a2907811df792603be0407d8
Bill Koch <koch.149@osu.edu>
2012-12-04 12:22:10 -0500

    Mass commit of the Rails generated controllers for Location and Interviewer

--- db/schema.rb
+++ db/schema.rb
@@ -22,6 +22,20 @@ ActiveRecord::Schema.define(:version => 20121204160454) do
     t.datetime "updated_at", :null => false
   end
 
+  create_table "interviewers", :force => true do |t|
+    t.string   "last_name"
+    t.string   "first_name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "locations", :force => true do |t|
+    t.datetime "time"
+    t.string   "conference_room"
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
+  end
+
   create_table "roles", :force => true do |t|
     t.string   "name"
     t.datetime "created_at", :null => false
